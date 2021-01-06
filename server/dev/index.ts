ed11d2872a03e3662c3689f9c993a3998acf41f3
Gregoire <gregoire@level-one.fr>
2021-01-06 09:37:27 +0100

    dev server update

--- server/dev/index.ts
+++ server/dev/index.ts
@@ -1,15 +1,17 @@
 import { Express } from "express";
 import { NODE_ENV } from "../../config/env";
 import { config } from "../config";
-const webpack = require("webpack");
-const webpackDevMiddleware = require("webpack-dev-middleware");
-const getWebpackConfig = require("../../webpack/webpack.config");
 
 export const devPath = (app: Express, express: any) => {
   if (NODE_ENV !== "development") return;
 
+  const getWebpackConfig = require("../../webpack/webpack.config");
+
   const webpackConfig = getWebpackConfig({}, { mode: "development" });
+  const webpack = require("webpack");
+
   const compiler = webpack(webpackConfig);
+  const webpackDevMiddleware = require("webpack-dev-middleware");
 
   app.use(
     webpackDevMiddleware(compiler, {
